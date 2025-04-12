36fa013f470ede6ce382092577169d8c56d8a15d
SebastijanT <79153493+strojanski@users.noreply.github.com>
2025-04-12 21:11:39 +0200

    Add final + report

--- project3/submission/main.py
+++ project3/submission/main.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-from sequence_utils import VOTSequence
-from ex3_utils import create_gauss_peak, create_cosine_window, gausssmooth
-import os 
-import time 
+# from sequence_utils import VOTSequence
+from ex3_utils import create_gauss_peak, create_cosine_window, gausssmooth,get_patch
+from utils.tracker import Tracker
+import time
 
 data_path = "vot2013/"
 
@@ -39,22 +39,31 @@ def show_img(img):
     plt.imshow(img)# cmap="gray")
     plt.show()
 
-class CorrelationFilter:
-    def __init__(self, alpha=0.25, lmbd=1e-8, sigma=np.sqrt(3)):
+class CorrelationFilter(Tracker):
+    def __init__(self, alpha=0.2, lmbd=1e-8, sigma=np.sqrt(3), increase_factor=1.3):
         self.alpha = alpha  
         self.lmbd = lmbd    
+        self.sigma = sigma
+        self.increase_factor = increase_factor
+        print(self.alpha, self.lmbd)
         
     def name(self):
-        return "CorrelationFilter"
+        return f"CorrelationFilter"#_{self.sigma:.2f}_{self.alpha}_{self.lmbd}"
 
     def initialize(self, image, bbox):
-        
+        start = time.time()
         if len(bbox) == 8:
             x_ = np.array(bbox[::2])
             y_ = np.array(bbox[1::2])
             bbox = [np.min(x_), np.min(y_), np.max(x_) - np.min(x_) + 1, np.max(y_) - np.min(y_) + 1]
         
         bbox = [int(i) for i in bbox]
+        inc_w = int(np.floor(bbox[2] * self.increase_factor))
+        inc_h = int(np.floor(bbox[3] * self.increase_factor))
+        bbox[0] = int(bbox[0] - (inc_w - bbox[2]) / 2)
+        bbox[1] = int(bbox[1] - (inc_h - bbox[3]) / 2)
+        bbox[2] = inc_w
+        bbox[3] = inc_h
         
         if bbox[2] % 2 == 0:
             bbox[2] += 1
@@ -68,14 +77,13 @@ class CorrelationFilter:
         self.target_size = (bbox[2], bbox[3])
         self.frame_ix = 1
         
-        # image = gausssmooth(image, 2)
         
-        # Initialize filter with Gaussian target
-        self.G, self.cosine = init(self.target_size, gauss_sigma=np.sqrt(2))
-        F = center_on_target(frame, self.bbox)
+        self.G, self.cosine = init(self.target_size, gauss_sigma=self.sigma)
+        F,_ = get_patch(frame, self.bbox[:2], self.bbox[2:])
         F = self.cosine * F
         
-        self.G_hat = np.fft.fft2(self.G)#[:-1, :-1]
+        
+        self.G_hat = np.fft.fft2(self.G)
         
         F = (F - np.mean(F)) / np.std(F)
         F_hat = np.fft.fft2(F)
@@ -83,23 +91,22 @@ class CorrelationFilter:
         
         
         self.H_hat_conj = (self.G_hat * F_hat_conj) / (F_hat * F_hat_conj + self.lmbd)
+        end = time.time()
         
+        # print("Initialization took", end - start)
 
     def track(self, frame):
+        start = time.time()
         frame = frame.astype(np.float32)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # frame = gausssmooth(frame, 2)
-        
-        # 1. Localization (Eq.2)
-        F = center_on_target(frame, self.bbox)
+        F,_ = get_patch(frame, self.bbox[:2], self.bbox[2:])
         F = self.cosine * F # Apply cosine window
         F = (F - np.mean(F)) / (np.std(F))
         
         F_hat = np.fft.fft2(F)
         R = np.fft.ifft2(F_hat * self.H_hat_conj)
         
-        # Find peak response
         max_pos = np.unravel_index(np.argmax(np.abs(R)), R.shape)
         
         w, h = R.shape[1], R.shape[0]
@@ -110,15 +117,13 @@ class CorrelationFilter:
         if y > h / 2:
             y = y - h
         
-        # Update bounding box
         self.bbox[0] += x
         self.bbox[1] += y
         
         self.bbox[0] = self.bbox[0].clip(0, frame.shape[1] - w)
         self.bbox[1] = self.bbox[1].clip(0, frame.shape[0] - h)
-        
-        # Update 
-        F_new = center_on_target(frame, self.bbox)
+    
+        F_new,_ = get_patch(frame, self.bbox[:2], self.bbox[2:])
         F_new = self.cosine * F_new
         F_new = (F_new - np.mean(F_new)) / (np.std(F_new))
         F_new_hat = np.fft.fft2(F_new)
@@ -127,10 +132,13 @@ class CorrelationFilter:
         
         H_new_hat_conj = (self.G_hat * F_new_hat_conj) / (F_new_hat * F_new_hat_conj + self.lmbd)
         self.H_hat_conj = (1 - self.alpha) * self.H_hat_conj + self.alpha * H_new_hat_conj
-        self.H_hat_conj /= np.max(np.abs(self.H_hat_conj))  
         self.H_hat_conj = self.H_hat_conj
 
-        return self.bbox
+        end = time.time()
+        
+        # print("Tracking the frame took", end- start)
+
+        return (self.bbox[0], self.bbox[1], self.bbox[2], self.bbox[3])#(self.bbox[0], self.bbox[1], self.target_size[0], self.target_size[1])
         
 
         
